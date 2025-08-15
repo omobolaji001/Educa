d9616e7679ade57878cf443582cf1bfe03159982
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-08-15 09:19:27 +0100

    Students registration form

---
+++ educa/students/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from courses.models import Course
+
+
+class CourseEnrollForm(forms.Form):
+    """ Handles the student's course enrollment """
+    course = forms.ModelChoiceField(
+        queryset=Course.objects.none(),
+        widget=forms.HiddenInput
+    )
+
+    def __init__(self, *args, **kwargs):
+        """ Initialization """
+        super(CourseEnrollForm, self).__init__(*args, **kwargs)
+        self.fields['course'].queryset = Course.objects.all()
