8cf1d6a0f6e241cc9d4d152274391d37162c3897
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-08-20 22:46:38 +0100

    Include a model to persist the messages to the database

---
+++ educa/chat/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+
+
+class Message(models.Model):
+    """ Saves messages to the database """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='chat_messages'
+    )
+    course = models.ForeignKey(
+        'courses.Course',
+        on_delete=models.PROTECT,
+        related_name='chat_messages'
+    )
+    content = models.TextField()
+    sent_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """ String representation """
+        return f'{self.user} on {self.course} at {self.sent_on}'
